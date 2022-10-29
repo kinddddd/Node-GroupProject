12451ee941b78690ab45dcfaa871364c91ac249b
Kind <alvaromoreno21197@gmail.com>
2022-10-29 12:27:15 +0200

    FEAT added db, movies model and folders

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const connectDb = require('./src/utils/db');
+
+connectDb();
+
+const PORT = process.env.PORT;
+const server = express();
+
+server.use(cors());
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
+
+
+
+
+
+
+
+
+
+
+
+server.use((error, req, res, next) => {
+    return res.status(error.status || 500).json(error.message || 'Unexpected error');
+});
+
+server.listen(PORT, () => {
+    console.log(`El servidor se ha iniciado en http://localhost:${PORT}`);
+});
